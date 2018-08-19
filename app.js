c5e019f261b931f3d9643562ec1238ab7b53bf66
Jeff <jeffersonkadams@gmail.com>
2018-08-19 19:51:53 -0400

    initialized initial variables, calculated dice rolls, began DOM manupulation.

--- app.js
+++ app.js
@@ -8,3 +8,15 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+var scores, roundScore, activePlayer, dice;
+
+scores = [0,0];
+roundScore = 0;
+activePlayer = 0;
+
+dice = Math.floor(Math.random() * 6) + 1; //random number from 1-6
+
+document.querySelector('#current-' + activePlayer).textContent = dice;
+//textContent instead of innerHTML because I'm not adding any extra HTML and it isn't necessary.
+
+document.querySelector('.dice').style.display = 'none';
